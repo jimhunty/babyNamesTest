b25791dbcde8104bb13b1baeb0e8b79c584100fc
James Miller <james@james-miller.co.uk>
2013-10-16 17:42:10 +0100

    Basic CURL post and response working

--- index.php
+++ index.php
@@ -11,6 +11,20 @@
 <body>
 	<div class="container">
 		<h1>Hello, world!</h1>
+		<?php
+			$url = 'http://www.socialsecurity.gov/cgi-bin/popularnames.cgi';
+			$myvars = 'year=1984&top=20';
+
+			$ch = curl_init( $url );
+			curl_setopt( $ch, CURLOPT_POST, 1);
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
+			curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt( $ch, CURLOPT_HEADER, 0);
+			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+
+			$response = curl_exec( $ch );
+			echo $response;
+		?>
 	</div>
 </body>
 	<script type="text/javascript" src="assets/css/bootstrap.min.js" />

50968a1b9227600ce0e607c7695f9e450296c897
James Miller <james@james-miller.co.uk>
2013-10-16 17:37:49 +0100

    Bring in the bootstrap template to allow future proof a good looking page.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Orange Digital Test</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="A web page that can aggregate the top 1000 male names between a given date range of one or more years" />
+	<meta name="keywords" content="Male Name Aggregator, Oragnge Digital, James Miller" />
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+</head>
+<body>
+	<div class="container">
+		<h1>Hello, world!</h1>
+	</div>
+</body>
+	<script type="text/javascript" src="assets/css/bootstrap.min.js" />
+</html>

0c53a27d6fc1badb1521eb424d45c52345fd6197
James Miller <james@james-miller.co.uk>
2013-10-16 19:24:22 +0100

    Commenting up the temporary function

--- libs/babyNames.php
+++ libs/babyNames.php
@@ -6,10 +6,13 @@ Date Created: 16/10/2013
 function getBabyNames($year){
 	// Class to select HTML DOM elements
 	include("htmldom/simple_html_dom.php");
-	$url = 'http://www.socialsecurity.gov/cgi-bin/popularnames.cgi';
-	$num = 1000;
-	$myvars = 'year='.$year.'&top='.$num;
 
+	// Constants
+	$url = 'http://www.socialsecurity.gov/cgi-bin/popularnames.cgi';
+	$num = 10;
+	$myvars = 'year='.$year.'&top='.$num.'&number=n';
+
+	// Initiate a CURL post
 	$ch = curl_init( $url );
 	curl_setopt( $ch, CURLOPT_POST, 1);
 	curl_setopt( $ch, CURLOPT_POSTFIELDS, $myvars);
@@ -17,26 +20,39 @@ function getBabyNames($year){
 	curl_setopt( $ch, CURLOPT_HEADER, 0);
 	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
 
+	// response is HTML, we now need to extract the data we want.
 	$response = curl_exec( $ch );
+	// Use the function within the simple HTML DOM Library to find objects
 	$html = str_get_html($response);
 
+	// Starting vairables for the process below
 	$i = 0;
 	$nomore = false;
 	$data = array();
 
+	// Find every table row witin the HTML page
 	foreach($html->find('tr') as $e){
 		$str = $e->innertext;
 		
+			// As the website is table based, we need to start at the point of the data
 		    if($i > 4){
+		    	// If statement used as a trigger to stop collecting data
 		    	if (strpos($str, 'Rank 1 is the most popular, rank 2 is the next most popular, and so forth') !== false){
 					$nomore = true;
 				}
 				if($nomore == false){
-		    		$data[] = explode(" ", trim($str));
+					
+					// Tidy data, split it and add it into an array
+		    		$names = explode(" ", trim($str));
+		    		$names[1] = str_replace(",", "", $names[1]);
+		    		preg_match_all('/^([^\d]+)(\d+)/', $names[1], $match);
+
+					$data[] = array($match[1][0], $match[2][0]);
 		    	}
 			}
 	$i++;
 	}	
+	// Return as a 2D array to be used in other functions
 	return $data;
 }
 ?>
